76421b372580da428f05722c99ffbd2a05125c13
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-28 15:50:56 +0200

    332

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    });
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
