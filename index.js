476a8b777d5942636ab1f61f177cb72b37ed3cf9
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-29 13:09:24 +0200

    334

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-require('dotenv').config();
-
-console.log( process.env );
 
 const app = express();
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         ok: true
-//     });
-// });
+app.use('/api/auth', require('./routes/auth'));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(4000, () => {
+    console.log(`Server is running on port 4000`);
 });
