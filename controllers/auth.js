63338e30920fca94fd524bc40c0e7d40f75bd959
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-03 08:57:22 +0200

    337

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response ) => {
 
     const { name, email, password } = req.body;
 
-    if ( name.length < 3 ) {
+    // handle errors
+    const errors = validationResult( req );
+    if ( !errors.isEmpty() ) {
         return res.status(400).json({
-            ok: false,
-            msg: 'Minimum 3 characters'
-        })
+            ok: 'false',
+            errors: errors.mapped()
+        });
     }
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'register',
         name,
@@ -23,6 +26,15 @@ const createUser = (req, res = response ) => {
 const loginUser = (req, res = response) => {
 
     const { email, password } = req.body;
+
+    // handle errors
+    const errors = validationResult( req );
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: 'false',
+            errors: errors.mapped()
+        });
+    }
 
     res.json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,29 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
+router.post(
+    '/new',
+    [ // middlewares
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password minimum length 6').isLength({ min: 6 }),
+    ],
+    createUser
+);
 
-router.post('/', loginUser);
+router.post(
+    '/',
+    [
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password minimum length 6').isLength({ min: 6 }),
+    ],
+    loginUser
+);
 
 router.get('/renew', renewToken);
 
