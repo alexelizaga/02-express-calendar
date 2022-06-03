38201bc8eca72d1b8d4613f8f4a55fbb26a7e639
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-03 06:35:14 +0200

    335

---
+++ controllers/auth.js
@@ -0,0 +1,34 @@
+const { response } = require('express');
+
+const createUser = (req, res = response ) => {
+
+    const { name, email, password } = req.body;
+
+    res.json({
+        ok: true,
+        msg: 'register',
+        name,
+        email,
+        password
+    });
+}
+
+const loginUser = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'login'
+    });
+};
+
+const renewToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
+    });
+};
+
+module.exports = {
+    createUser,
+    loginUser,
+    renewToken
+}
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+// Public directory
+app.use( express.static('public') );
 
-app.use('/api/auth', require('./routes/auth'));
+// Read and Parse JSON
+app.use( express.json() );
 
+// Routes
+app.use( '/api/auth', require('./routes/auth') );
+
+// Listen for requests
 app.listen(4000, () => {
     console.log(`Server is running on port 4000`);
 });
