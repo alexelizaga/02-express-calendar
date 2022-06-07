61b53a2c3629c92ff4aba81498218390ee436c41
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-07 07:29:46 +0200

    345

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const generateJWT = ( uid, name ) => {
-    const token = jwt.sign({
-        uid,
-        name
-    }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES
-    });
 
     return new Promise( (resolve, reject) => {
 
         const payload = { uid, name };
 
-        jwt.sign( payload, process.env.JWT_SECRET, {})
+        jwt.sign( payload, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES
+        }, (err, token) => {
+            if (err) {
+                reject(err);
+            };
+            resolve(token);
+        });
+
     });
 }
 
