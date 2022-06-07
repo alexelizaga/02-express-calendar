0882dd32b77e1579766307ca806d1ca0e3d7e024
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-07 12:57:44 +0200

    357

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { request, response } = require('express');
 const Event = require('../models/Event');
 
 const getEvents = async (req, res = response) => {
@@ -37,12 +37,48 @@ const createEvent = async (req, res = response) => {
 
 };
 
-const updateEvent = (req, res = response) => {
+const updateEvent = async (req = request, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    });
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const event = await Event.findById( eventId );
+
+        if ( !event ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Event not found'
+            });
+        };
+
+        if ( event.user.toString() !== uid ) {
+            res.status(401).json({
+                ok: false,
+                msg: 'Not authorized'
+            });
+        };
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+        res.json({
+            ok: true,
+            event: eventUpdated
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error in update event'
+        });
+    }
 
 };
 
--- routes/events.js
+++ routes/events.js
@@ -32,6 +32,12 @@ router.post(
 
 router.put(
     '/:id',
+    [
+        check('title', 'Title is required').not().isEmpty(),
+        check('start', 'Start date is required').custom( isDate ),
+        check('end', 'End date is required').custom( isDate ),
+        fieldValidator
+    ],
     updateEvent
 );
 
