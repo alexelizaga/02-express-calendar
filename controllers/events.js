c430e5f58e12a37ad171155879643b4d41562327
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-07 12:28:52 +0200

    355

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 const getEvents = (req, res = response) => {
 
@@ -9,14 +10,28 @@ const getEvents = (req, res = response) => {
 
 };
 
-const createEvent = (req, res = response) => {
+const createEvent = async (req, res = response) => {
 
-    console.log(req.body);
+    const event = new Event( req.body );
 
-    res.json({
-        ok: true,
-        msg: 'createEvent'
-    });
+    try {
+
+        event.user = req.uid;
+
+        const eventSaved = await event.save();
+
+        res.json({
+            ok: true,
+            eventSaved
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error in create event'
+        });
+    }
 
 };
 
